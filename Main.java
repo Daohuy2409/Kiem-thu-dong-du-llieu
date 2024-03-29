d8e257352cc39c640fb76b75d4b9e9dce5b236e7
Daohuy2409 <100114549+Daohuy2409@users.noreply.github.com>
2024-03-29 19:50:04 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,15 +9,16 @@ public class Main {
         System.out.println(commonDivisors(a, b));
     }
     public static int commonDivisors(int a, int b) {
+        if (a * b == 0) {
+            return 0;
+        }
         if (a < 0) {
             a = -a;
         }
         if (b < 0) {
             b = -b;
         }
-        if (a * b == 0) {
-            return 0;
-        }
+
         int n = 0;
         for (int i = 1; i <= a && i <= b; i++) {
             if (a % i == 0 && b % i == 0) {
